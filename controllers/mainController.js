fa9742a6be47bc5d0e96254f69e9bb9edc3077fe
spegisima <agneta.smergelyte@gmail.com>
2023-09-15 16:48:43 +0300

    users added

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -8,16 +8,14 @@ const serviceDb = require("../schemas/servicesSchema");
 module.exports = {
     addNewService: async (req, res) => {
         const {name, price, description} = req.body;
-        console.log(name, price, description)
         const service = new serviceDb({
             name,
             price: Number(price),
             description
         });
-        console.log(service)
         try {
             await service.save();
-            resSend(res, false, service, null);
+            resSend(res, false, null, null);
         } catch (err) {
             resSend(res, true, null, 'Failed to save')
         }
@@ -26,6 +24,42 @@ module.exports = {
         try {
             const allServices = await serviceDb.find();
             resSend(res, false, allServices, null);
+        } catch (err) {
+            resSend(res, true, null, 'Server error')
+        }
+    },
+    deleteService: async (req, res) => {
+        const {id} = req.params
+        try {
+            const serviceToDelete = await serviceDb.findOneAndDelete({_id : id});
+            if (serviceToDelete) {
+                resSend(res, false, null, 'Membership deleted');
+            } else {
+                resSend(res, true, null, 'Deletion failed');
+            }
+        } catch (err) {
+            resSend(res, true, null, 'Server error');
+        }
+    },
+    addNewUser: async (req, res) => {
+        const {name, surname, email, service_id} = req.body;
+        const user = new userDb({
+            name,
+            surname,
+            email,
+            service_id
+        })
+        try {
+            await user.save();
+            resSend(res, false, null, null);
+        } catch (err) {
+            resSend(res, true, null, 'Failed to save')
+        }
+    },
+    getAllUsers: async (req, res) => {
+        try {
+            const allUsers = await userDb.find();
+            resSend(res, false, allUsers, null);
         } catch (err) {
             resSend(res, true, null, 'Server error')
         }
--- router/mainRouter.js
+++ router/mainRouter.js
@@ -3,14 +3,16 @@ const router = express.Router();
 
 const {
     getAllServices,
-    addNewService
+    addNewService,
+    deleteService,
+    addNewUser,
+    getAllUsers
 } = require("../controllers/mainController");
 
-
-router.get("/memberships", getAllServices)
-router.post("/memberships", addNewService)
-
-
-
+router.get("/memberships", getAllServices);
+router.post("/memberships", addNewService);
+router.delete("/memberships/:id", deleteService);
+router.get("/users", getAllUsers)
+router.post("/users", addNewUser);
 
 module.exports = router;
