caeedd954191bda3fe3648d4fb02483a7bde63f1
spegisima <agneta.smergelyte@gmail.com>
2023-11-03 10:03:59 +0200

    cors changes

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const cors = require("cors");
 const router = require("./router/mainRouter");
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DB_KEY);
-index.use(cors());
+index.use(cors({
+    origin: 'https://memberships.onrender.com'
+}));
 index.use(express.json());
 index.use("/", router);
 index.listen(port);
